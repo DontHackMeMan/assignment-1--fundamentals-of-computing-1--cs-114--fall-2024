3211d7b5e66dab1c9eaf90a82589aa32e30d8dce
DontHackMeMan <cjfludx@gmail.com>
2024-09-18 18:05:58 -0400

    Update Assignment1.java
    Initials done

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,19 @@
 // Import libraries here
+import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+    System.out.println("      CCCCCCCCC      FFFFFFFFF     ");
+    System.out.println("    CC         C     FF     ");
+    System.out.println("   CC                FF     ");
+    System.out.println("   CC                FF     ");
+    System.out.println("   CC                FFFFFFFF     ");
+    System.out.println("   CC                FF     ");
+    System.out.println("   CC                FF     ");
+    System.out.println("    CC         C     FF     ");
+    System.out.println("      CCCCCCCCC      FF     ");
+    Scanner scan = new Scanner(System.in);
+
   }
 }
