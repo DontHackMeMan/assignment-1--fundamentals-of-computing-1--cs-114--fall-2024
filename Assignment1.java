e4ddfe8541e64bfb004820c01d80f054a2c39603
DontHackMeMan <cjfludx@gmail.com>
2024-09-18 19:28:47 -0400

    Update Assignment1.java
    Added Temperature Converter

--- Assignment1.java
+++ Assignment1.java
@@ -13,7 +13,17 @@ public class Assignment1 {
     System.out.println("   CC                FF     ");
     System.out.println("    CC         C     FF     ");
     System.out.println("      CCCCCCCCC      FF     ");
-    Scanner scan = new Scanner(System.in);
 
+    final int BASE = 32;
+    final double Conversion = 1.8;
+    double temperature, celcius;
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Enter a temperature measurement This will be converted from fahrenheit to celcius. ");
+    temperature = scan.nextDouble();
+
+    celcius =  (temperature-BASE) /(Conversion) ;
+    System.out.println("Temperature in Celsius:"+ celcius);
+    scan.close();
   }
 }
