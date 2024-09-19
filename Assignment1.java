7e10de10150eb99033427d4c97cd42bfe15d3bde
DontHackMeMan <cjfludx@gmail.com>
2024-09-18 20:54:17 -0400

    Added random number

--- Assignment1.java
+++ Assignment1.java
@@ -1,6 +1,7 @@
 // Import libraries here
 import java.util.Scanner;
-
+import java.util.Random;
+;
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
@@ -16,14 +17,28 @@ public class Assignment1 {
 
     final int BASE = 32;
     final double Conversion = 1.8;
-    double temperature, celcius;
+    double temperature, celsius;
     Scanner scan = new Scanner(System.in);
+    Random generator = new Random();
+    String userstring, cut;
+    int num;
+
     
     System.out.println("Enter a temperature measurement This will be converted from fahrenheit to celcius. ");
     temperature = scan.nextDouble();
 
-    celcius =  (temperature-BASE) /(Conversion) ;
-    System.out.println("Temperature in Celsius:"+ celcius);
+    celsius =  (temperature-BASE) /(Conversion) ;
+    System.out.println();
+
+    System.out.println("Enter a 5-character string");
+    userstring = scan.next();
+    StringBuffer flip = new StringBuffer(userstring);
+    flip.reverse();
+    cut = flip.substring(1, 4);
+    
+    num = generator.nextInt(16352) + 32;
+    
+    System.out.println("Your new string is "+celsius+cut+num);
     scan.close();
   }
 }
